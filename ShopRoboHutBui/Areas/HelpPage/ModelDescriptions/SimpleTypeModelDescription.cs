a2a2e100abdca3f0fb03ac533c483f5399e579e7
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-04-22 22:07:23 +0700

    0.

---
+++ ShopRoboHutBui/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ShopRoboHutBui.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ShopRoboHutBui/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopRoboHutBui.WebApiApplication" Language="C#" %>
